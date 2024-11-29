4c16a6489b01ff0c30b8bb36c8c0b7acc525fa2c
cxp <codexpoint@gmail.com>
2024-11-29 15:22:33 +0530

    done

--- app/(home)/_components/Testimonial.tsx
+++ app/(home)/_components/Testimonial.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Testimonial = () => {
   return (
-    <div className="bg-gradient-to-r from-[#e3f2fd] to-[#bbdefb] py-10 mx-auto h-auto items-center  justify-center w-full flex flex-col ">
+    <div className="bg-gradient-to-r from-[#e3f2fd] to-[#bbdefb] py-10 mx-auto h-auto items-center gap-4  justify-center w-full flex flex-col ">
       <h2 className="text-4xl font-bold text-[#333333]">Testimonials</h2>
 
-      <div className="bg-white w-1/2 rounded-md h-[50vh]">shrey</div>
+      <div className="bg-white w-1/2 rounded-md shadow-md  h-[50vh]"></div>
     </div>
   );
 };
