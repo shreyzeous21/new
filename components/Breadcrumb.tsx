9e29e5c2ea18db72712522946cc45c9a48e58f64
cxp <codexpoint@gmail.com>
2024-11-30 15:21:56 +0530

    done

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -12,33 +12,35 @@ const Breadcrumb = () => {
   }
 
   return (
-    <nav aria-label="breadcrumb" className="py-1 px-4 bg-gray-200">
-      <ol className="flex space-x-2 text-sm">
-        <li>
-          <Link href="/" className="text-blue-600 hover:underline">
-            Home
-          </Link>
-        </li>
-        {pathSegments.map((segment, index) => {
-          const isLast = index === pathSegments.length - 1;
-          const href = "/" + pathSegments.slice(0, index + 1).join("/");
+    <nav aria-label="breadcrumb" className=" bg-gray-200">
+      <div className="container mx-auto justify-center px-10">
+        <ol className="flex space-x-2 text-sm">
+          <li>
+            <Link href="/" className="text-blue-600 hover:underline">
+              Home
+            </Link>
+          </li>
+          {pathSegments.map((segment, index) => {
+            const isLast = index === pathSegments.length - 1;
+            const href = "/" + pathSegments.slice(0, index + 1).join("/");
 
-          return (
-            <li key={index} className="flex items-center space-x-2">
-              <span>/</span>
-              {isLast ? (
-                <span className="text-gray-500">
-                  {decodeURIComponent(segment)}
-                </span>
-              ) : (
-                <Link href={href} className="text-blue-600 hover:underline">
-                  {decodeURIComponent(segment)}
-                </Link>
-              )}
-            </li>
-          );
-        })}
-      </ol>
+            return (
+              <li key={index} className="flex items-center space-x-2">
+                <span>/</span>
+                {isLast ? (
+                  <span className="text-gray-500">
+                    {decodeURIComponent(segment)}
+                  </span>
+                ) : (
+                  <Link href={href} className="text-blue-600 hover:underline">
+                    {decodeURIComponent(segment)}
+                  </Link>
+                )}
+              </li>
+            );
+          })}
+        </ol>
+      </div>
     </nav>
   );
 };
