f9e4e60e81c8ae3a8f8598a7e38cb2b9c5dddd1a
cxp <codexpoint@gmail.com>
2024-11-30 15:41:46 +0530

    done

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -13,7 +13,7 @@ const Breadcrumb = () => {
 
   return (
     <nav aria-label="breadcrumb" className=" bg-gray-200">
-      <div className="container mx-auto justify-center px-3">
+      <div className="container mx-auto justify-center ">
         <ol className="flex space-x-2 text-sm">
           <li>
             <Link href="/" className="text-blue-600 hover:underline">
