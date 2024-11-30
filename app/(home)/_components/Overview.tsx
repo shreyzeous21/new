d2ebc30c098ed960266a599a21bbe5b99d59836a
cxp <codexpoint@gmail.com>
2024-11-30 15:03:17 +0530

    done

--- app/(home)/_components/Overview.tsx
+++ app/(home)/_components/Overview.tsx
@@ -6,13 +6,13 @@ const Overview = () => {
     <div className="bg-white h-full lg:mx-auto lg:w-full mx-4 max-w-7xl py-16 flex flex-col">
       <div className="flex lg:flex-row flex-col justify-center items-center w-full mx-auto  gap-4 ">
         <div
-          className="lg:w-1/2 w-full text-left flex flex-col h-full  gap-5
+          className="lg:w-1/2 w-full text-left gap-3 flex flex-col h-full
         "
         >
           <h2 className="text-4xl text-[#243765] font-semibold">
             No One Cools Data Centers Like GRC
           </h2>
-          <p className="text-black text-xl">
+          <p className=" text-black text-lg">
             That’s the cold truth. No other company has the history, global
             presence or expertise in liquid immersion cooling that we do. GRC
             has been breaking through limitations and setting new standards of
