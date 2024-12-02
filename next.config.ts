13849609090644df9c924d4d77701aee989139dc
cxp <codexpoint@gmail.com>
2024-12-02 21:03:37 +0530

    done

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   useFileSystemPublicRoutes: false,
+  output: "export",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: ["www.grcooling.com"],
   },
