d295dcbdd19bce1fd06ced1d6ba4d9a4176e1b1e
cxp <codexpoint@gmail.com>
2024-12-03 11:23:08 +0530

    done

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["www.grcooling.com"],
+    remotePatterns: [{ protocol: 'https', hostname: 'www.grcooling.com' }],
   },
   // output: "export",
   typescript: {
